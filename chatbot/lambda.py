59a6d4221bb3237dfd3e2ec385c18e4391d07033
Eddie <eddie@Olgas-MacBook-Pro.local>
2024-07-30 14:04:44 -0400

    feat: cors origins

--- chatbot/lambda.py
+++ chatbot/lambda.py
@@ -2,7 +2,14 @@ import boto3
 
 
 def handler(event, context):
+    cors_allow_origins = [
+        'http://localhost:1313',
+        'https://eddiecorrigall.github.io'
+    ]
     return {
         'statusCode' : 200,
+        'headers': {
+            'Access-Control-Allow-Origin': ','.join(cors_allow_origins)
+        },
         'body': 'Hello World'
     }
