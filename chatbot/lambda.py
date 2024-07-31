8a5619aec3a375cea05d5c7d61e8b890d10d2c7d
Eddie <eddie@Olgas-MacBook-Pro.local>
2024-07-30 21:04:08 -0400

    chore: refactor test conversation

--- chatbot/lambda.py
+++ chatbot/lambda.py
@@ -4,23 +4,25 @@ import traceback
 
 bedrock = boto3.client(service_name='bedrock-runtime')
 
-def get_chatbot_response(request):
+system_prompt = (
+    "You are an app that creates playlists for a radio station that plays rock and pop music."
+    "Only return song names and the artist."
+)
+
+def get_chatbot_response(messages):
     # https://aws.amazon.com/blogs/aws/announcing-llama-3-1-405b-70b-and-8b-models-from-meta-in-amazon-bedrock/
     # https://docs.aws.amazon.com/bedrock/latest/userguide/conversation-inference.html
-    conversation = [
-        {
+    conversation = []
+    for message in messages:
+        conversation.append({
             'role': 'user',
-            'content': [{'text': request}],
-        },
-        {
-            'role': 'assistant',
-            'content': [{'text': 'You are a helpful assistant.'}]
-        }
-    ]
+            'content': [{'text': message}],
+        })
     response = bedrock.converse(
         # modelId='meta.llama3-1-405b-instruct-v1:0',
         modelId='meta.llama3-70b-instruct-v1:0',
         # modelId='meta.llama3-8b-instruct-v1:0',
+        system=[{"text": system_prompt}],
         messages=conversation,
         inferenceConfig={'maxTokens': 512, 'temperature': 0.5, 'topP': 0.9},
     )
@@ -32,10 +34,12 @@ def lambda_handler(event, context):
         'http://localhost:1313',
         'https://eddiecorrigall.github.io'
     ]
-    request = "Describe the purpose of a 'hello world' program in one line."
+    messages = [
+        'Create a list of 3 pop songs.'
+    ]
     response = ''
     try:
-        response = get_chatbot_response(request)
+        response = get_chatbot_response(messages)
     except Exception as e:
         return {
             'errorType': 'InternalServerError',
