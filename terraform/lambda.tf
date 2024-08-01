0e18411ae34e4eefb72c5d447dc9bf5a5ae60198
Eddie <eddie@Olgas-MacBook-Pro.local>
2024-08-01 09:51:54 -0400

    fix: integration method must be POST

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -125,7 +125,7 @@ resource "aws_apigatewayv2_integration" "chatbot" {
 
   integration_uri    = aws_lambda_function.chatbot.invoke_arn
   integration_type   = "AWS_PROXY"
-  integration_method = "ANY"
+  integration_method = "POST"
 }
 
 resource "aws_apigatewayv2_route" "chatbot_message" {
