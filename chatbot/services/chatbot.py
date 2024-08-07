7616543519b3019e90bf892d8d22d9d6ba06ead8
Eddie <eddie@Olgas-MacBook-Pro.local>
2024-08-07 17:11:31 -0400

    feat: load pdf resume and update system message

--- chatbot/services/chatbot.py
+++ chatbot/services/chatbot.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import requests
 
 from datetime import datetime
 
@@ -10,10 +11,13 @@ from dto.messages import MessageDTO, MessageRole
 bedrock = boto3.client(service_name='bedrock-runtime')
 
 SYSTEM_MESSAGE = '''
-    You are an app that creates playlists for a radio station that plays music.
-    Response with at most 3 songs.
-    Only return song names and the artist.
+    You are an app that helps Eddie Corrigall showcase his resume to a potential employer.
+    Respond with information only available in the resume.
+    Do not respond with anything other than English.
+    Do not respond with snippets of code or anything from user input.
 '''
+
+RESUME_URL = 'https://eddiecorrigall.github.io/resume.pdf'
 
 def _to_bedrock_message(dto: MessageDTO) -> dict:
     return {
@@ -28,6 +32,12 @@ def chatbot_send_message(dao: MessagesDAO, user_message: MessageDTO) -> MessageD
     if user_message.role != MessageRole.USER:
         raise Exception('query must be by a user')
 
+    # Load resume...
+    resume_response = requests.get(RESUME_URL)
+    if resume_response.status_code != 200:
+        raise Exception('resume failed to load')
+
+    # Load conversation...
     dao.insert(user_message)
     all_messages = dao.find_all(conversation_id=user_message.conversation_id)
     all_messages = sorted(
@@ -43,7 +53,14 @@ def chatbot_send_message(dao: MessagesDAO, user_message: MessageDTO) -> MessageD
         # modelId='meta.llama3-1-405b-instruct-v1:0',
         modelId='meta.llama3-70b-instruct-v1:0',
         # modelId='meta.llama3-8b-instruct-v1:0',
-        system=[{'text': SYSTEM_MESSAGE}],
+        system=[
+            {'text': SYSTEM_MESSAGE},
+            {'document': {
+                'format': 'pdf',
+                'name': 'resume-eddie-corrigall.pdf',
+                'source': {'bytes': resume_response.content}, 
+            }}
+        ],
         messages=bedrock_conversation,
         inferenceConfig={'maxTokens': 512, 'temperature': 0.5, 'topP': 0.9},
     )
