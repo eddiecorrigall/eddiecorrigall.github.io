30a33345eb2d0eb773eefa68323ba1e038a35538
Eddie <eddie@Olgas-MacBook-Pro.local>
2024-08-13 14:16:30 -0400

    chore: setup dev for hugo and js

--- js/src/index.ts
+++ js/src/index.ts
@@ -3,7 +3,8 @@ import { createApp } from 'vue';
 import Test from './components/Test';
 
 const APPS = {
-    chatbot: (selector: string) => {
+    chatbot: (options: { api: string, selector: string }) => {
+        const { api, selector } = options;
         console.log('Loading chatbot')
         const app = createApp(Test, { message: 'yup' });
         app.mount(selector);
